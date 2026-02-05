4ebc5a0e2ae9209a6a7cc803f922852d88d3ed11
supportbstteam <support@betasofttechnology.com>
2026-02-05 17:58:24 +0530

    refactor added the code in teh accout/page.jsx

---
+++ src/app/(home)/account/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AccountPage() {
+  return (
+    <div className="min-h-screen p-10">
+      <h1 className="text-2xl font-semibold">
+        Account Page
+      </h1>
+    </div>
+  );
+}
